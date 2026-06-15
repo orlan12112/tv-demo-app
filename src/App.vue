4491b3c323a73f6ce7498f590bc0a1bace81e562
orlan12112 <orlan12112@yandex.ru>
2026-06-15 23:06:33 +0300

    доработка под требования кр

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
-  <div id="app">
+  <div id="app" class="mainApp">
     <AppNavbar v-if="userStore.isAuthenticated" />
     <router-view />
   </div>
+  <AppFooter v-if="userStore.isAuthenticated"/>
 </template>
 
 <script setup lang="ts">
-  import AppNavbar from './shared/ui/AppNavbar.vue';
+  import AppFooter from './shared/ui/AppFooter.vue';
+import AppNavbar from './shared/ui/AppNavbar.vue';
   import { useUserStore } from '@/entities/user/model/userStore';
 
   const userStore = useUserStore();
   
 </script>
+
+<style scoped>
+.mainApp {
+  display: flex;
+  flex-direction: column;
+  min-height: calc(100vh - 44px);
+}
+</style>
---
+++ src/shared/ui/AppFooter.vue
@@ -0,0 +1,18 @@
+<template>
+	<footer class="footer"  bg-dark>
+		© Кузнецов ЛИ ЗИИ-431
+	</footer>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="css">
+	.footer {
+		padding: 10px 10px;
+		background-color: #212529;
+		color: #fff !important;
+		font-weight: bold;
+	}
+</style>
